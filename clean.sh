234905edb40e77b7ae1442adb2488415639906e6
udit01 <uditjainiitd@gmail.com>
2018-04-26 16:15:19 +0530

    Put the basic model in place

---
+++ clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Find the suitable syntax then make
+# if [ -e *.aux ] ; then rm *.aux fi 
+# if [ -e *.log ] ; then rm *.log fi
+# if [ -e *.out ] ; then rm *.out fi
+# if [ -e *.gz  ] ; then rm *.gz  fi
+# if [ -e *.toc ] ; then rm *.toc fi
